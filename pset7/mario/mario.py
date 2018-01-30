b71ed6b510cc7808d4fa955e0a66f417a132163a
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2018-01-30 21:21:48 +0000

    Playing with mario

--- pset7/mario/mario.py
+++ pset7/mario/mario.py
@@ -1,23 +1,15 @@
 from cs50 import get_int
 
-def printSpaces(count):
-    printItem(' ', count)
-
-def printHashes(count):
-    printItem('#', count)
-
-def printItem(character, count):
-    for j in range(count):
-        print(character, end='')
-
 x = -1
+# get value from the user
 while x > 23 or x < 0:
     x = get_int("Height: ")
 
+# print the stairs
 for i in range(x):
     spaces = x - i - 1
     hashes = x - spaces + 1
 
-    printSpaces(spaces)
-    printHashes(hashes)
+    print(" " * spaces, end="")
+    print("#" * hashes, end="")
     print("")
