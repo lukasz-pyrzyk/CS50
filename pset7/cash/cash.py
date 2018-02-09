41f34be96aae61bae970bc687ae899e3b9921b88
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2018-02-09 23:41:45 +0100

    Bugfix for cash

--- pset7/cash/cash.py
+++ pset7/cash/cash.py
@@ -7,18 +7,18 @@ x = -1
 while x < 0:
     x = get_float("Change: ")
 
-    coints = [0.25, 0.1, 0.05, 0.01]
-    count = 0
-    i = 0
-    while i < 4:
-        coin = coints[i]
-        value = roundNumber(x)
-        if value >= coin:
-            x -= coin
-            count += 1
-            # print(f"Using {coin}")
-        else:
-            # print(f"Cannot use {coin}, current value is {value}")
-            i += 1
+coints = [0.25, 0.1, 0.05, 0.01]
+count = 0
+i = 0
+while i < 4:
+    coin = coints[i]
+    value = roundNumber(x)
+    if value >= coin:
+        x -= coin
+        count += 1
+        # print(f"Using {coin}")
+    else:
+        # print(f"Cannot use {coin}, current value is {value}")
+        i += 1
 
-    print(count)
+print(count)
