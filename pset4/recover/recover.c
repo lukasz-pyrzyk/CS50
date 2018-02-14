c135afb9d67a2564a93380b9c45ce3bb5a58845e
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2018-02-14 22:05:15 +0100

    More checks

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -3,7 +3,7 @@
 #include <stdint.h>
 #include <string.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
     if(argc != 2)
     {
@@ -18,15 +18,22 @@ int main(int argc, string argv[])
         printf("Unable to open file %s", fileName);
         return 2;
     }
-    
+
     int count = 0;
     uint8_t buffer[512];
     uint8_t jpegStartBuffer[4] = {0xff, 0xd8, 0xff, 0xe0};
+    uint8_t jpegEndBuffer[4] = {0xff, 0xd8, 0xff, 0xe1};
+    uint8_t check[8];
 
     FILE* outputFile = NULL;
-    while(fread(buffer, 1, 512, inputFile) == 512)
+    while(fread(buffer, 512, 1, inputFile) > 0)
     {
-        if (memcmp(buffer, jpegStartBuffer, 4) == 0)
+        for(int i = 0; i < 4; i++)
+		{
+			check[i] = buffer[i];
+		}
+
+        if ((memcmp(jpegStartBuffer, check, 4) == 0 ) || (memcmp(jpegEndBuffer, check, sizeof(check)) == 0))
         {
             char newFileName[8];
             sprintf(newFileName, "%03d.jpg", count);
@@ -34,16 +41,23 @@ int main(int argc, string argv[])
             if (outputFile == NULL)
             {
                 outputFile = fopen(newFileName, "w");
-                fwrite(buffer, sizeof(buffer), 512, outputFile);
+                fwrite(buffer, sizeof(buffer), 1, outputFile);
             }
             else
             {
                 fclose(outputFile);
                 outputFile = fopen(newFileName, "w");
-                fwrite(buffer, sizeof(buffer), 512, outputFile);
+                fwrite(buffer, sizeof(buffer), 1, outputFile);
                 count++;
             }
         }
+        else
+		{
+			if(outputFile != NULL)
+			{
+				fwrite(buffer, sizeof(buffer), 1, outputFile);
+			}
+		}
     }
     if (outputFile != NULL)
     {
